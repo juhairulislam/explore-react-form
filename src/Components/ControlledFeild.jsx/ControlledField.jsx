03c1832bc0cb2f8761cb879dbcf6caf29413480b
juhairulislam <juhairulislam2018@gmail.com>
2026-04-05 08:16:58 +0600

    controll and uncontrolled ways to collect form data

--- src/Components/ControlledFeild.jsx/ControlledField.jsx
+++ src/Components/ControlledFeild.jsx/ControlledField.jsx
@@ -3,10 +3,21 @@ import React, { useState } from 'react';
 const ControlledField = () => {
 
     const [password , setPassword] = useState('') ;
+    const [error, setError] = useState('')
 
     const handleOnChange = e =>{
-        console.log(e.target.value)
+        console.log(e.target.value);
+        setPassword(e.target.value); 
+
+        if(password.length < 6) {
+            setError('Password must br at least 6 character')
+        }else{
+            setError('')
+        }
+
     }
+
+
 
     const handleSubmit = (e) =>{
 
@@ -15,12 +26,16 @@ const ControlledField = () => {
         <div>
 
             <form onSubmit={handleSubmit}>
-                <input type="email" name="email" id="" required placeholder='Your Email' />
+                <input type="email" name="email" required placeholder='Your Email' />
                 <br />
-                <input type="password" name="password" onChange={handleOnChange} defaultValue={password} id="" placeholder='Password' required/>
+                <input type="password" name="password" onChange={handleOnChange} defaultValue={password}  placeholder='Password' required/>
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+
+            <p style={{color:'red'}}>
+                <small>{error}</small>
+            </p>
             
         </div>
     );
